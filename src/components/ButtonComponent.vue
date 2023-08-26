b8bfeff933727fc5ae8d98d4b1b2563e61e2df67
Matthias Winkler <github@m-winkler.at>
2023-08-26 16:44:17 +0200

    Add ButtonEditorPopover

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,49 +1,52 @@
 <script setup lang="ts">
-import { nextTick, ref } from 'vue';
+import { computed, nextTick, ref } from 'vue';
 import EditButton from './EditButton.vue';
+import ButtonEditorPopover from './modals/ButtonEditorPopover.vue';
 
+const isPopoverOpen = ref(false);
 const buttonText = ref("Header");
-const input = ref<HTMLElement | null>(null);
+const buttonUrl = ref("https://www.google.com");
+const button = ref<HTMLButtonElement | null>(null);
+const popoverPosition = computed(() => {
+    return {
+        left: button.value?.getBoundingClientRect().left ?? 0,
+        top: button.value?.getBoundingClientRect().bottom ?? 0 + 12
+    };
+});
 
 function edit() {
-    nextTick(() => {
-        if (!input.value) {
-            return;
-        }
+    isPopoverOpen.value = true;
+}
 
-        input.value?.focus();
-        const range = document.createRange();
-        const sel = window.getSelection();
-
-        range.selectNodeContents(input.value);
-        range.collapse(false);
-
-        sel?.removeAllRanges();
-        sel?.addRange(range);
-    })
+function updateButton({ newButtonText, newButtonUrl }: { newButtonText: string, newButtonUrl: string }) {
+    buttonText.value = newButtonText;
+    buttonUrl.value = newButtonUrl;
+    isPopoverOpen.value = false;
 }
 </script>
 
 <template>
-    <div class="group">
-        <button class="button relative px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md"
-                @click="edit()">
-            <edit-button @toggleEditMode="edit()"></edit-button>
-            <div ref="input"
-                 contentEditable="true">{{ buttonText }}</div>
-        </button>
+    <div>
+        <div class="group">
+            <button ref="button"
+                    class="button relative px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md"
+                    @click="edit()">
+                <EditButton @toggleEditMode="edit()"></EditButton>
+                {{ buttonText }}
+            </button>
+        </div>
+
+        <ButtonEditorPopover
+            v-if="isPopoverOpen"
+            :buttonText="buttonText"
+            :buttonUrl="buttonUrl"
+            :popoverPosition="popoverPosition"
+            @update="updateButton"
+            @close="isPopoverOpen = false"
+        />
     </div>
 </template>
 
 <style scoped lang="postcss">
-input {
-    border: none;
-    margin: 0;
-    padding: 0;
-    background: none;
-    font: inherit;
-    color: inherit;
-    appearance: none;
-    outline: none;
-}
+
 </style>
