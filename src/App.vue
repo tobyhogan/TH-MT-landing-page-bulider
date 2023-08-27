1c66aa62cabac5bfde89732fa96c8ab7d62da411
Toby Hogan <tobyhogan44@gmail.com>
2023-08-27 22:41:00 +0100

    added vue files

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style lang="postcss">
+#app {
+    @apply text-gray-100 subpixel-antialiased w-screen h-screen;
+}
+
+html,
+body {
+    @apply bg-gray-900 w-screen h-screen overflow-hidden;
+}
+</style>
---
+++ src/views/EditorView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import TextComponent from "@/components/TextComponent.vue";
+import ButtonComponent from "@/components/ButtonComponent.vue";
+import ImageComponent from "@/components/ImageComponent.vue";
+import NavbarComponent from "@/components/NavbarComponent.vue";
+</script>
+
+<template>
+  <NavbarComponent></NavbarComponent>
+  <main>
+    <div class="container flex flex-wrap p-8 mx-auto xl:px-0 max-width">
+        <div class="flex items-center w-full lg:w-1/2">
+          <div class="max-w-2xl mb-8">
+            <HeaderComponent></HeaderComponent>
+            <p>
+              <TextComponent></TextComponent>
+            </p>
+
+            <div class="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+              <ButtonComponent></ButtonComponent>              
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-center w-full lg:w-1/2">
+          <ImageComponent></ImageComponent>
+        </div>
+      </div>     
+  </main>
+</template>
