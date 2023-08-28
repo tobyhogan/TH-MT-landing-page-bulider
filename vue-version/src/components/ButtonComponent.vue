9eaae42a610ad7054583100b5656d8b4e508cd5d
Matthias Winkler <github@m-winkler.at>
2023-08-28 19:25:29 +0200

    Fix leftover merge changes

--- vue-version/src/components/ButtonComponent.vue
+++ vue-version/src/components/ButtonComponent.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-<<<<<<< HEAD
 import { computed, nextTick, ref } from 'vue';
 import EditButton from './EditButton.vue';
 import ButtonEditorPopover from './modals/ButtonEditorPopover.vue';
@@ -24,35 +23,10 @@ function updateButton({ newButtonText, newButtonUrl }: { newButtonText: string, 
     buttonText.value = newButtonText;
     buttonUrl.value = newButtonUrl;
     isPopoverOpen.value = false;
-=======
-import { nextTick, ref } from 'vue';
-import EditButton from './EditButton.vue';
-
-const buttonText = ref("Header");
-const input = ref<HTMLElement | null>(null);
-
-function edit() {
-    nextTick(() => {
-        if (!input.value) {
-            return;
-        }
-
-        input.value?.focus();
-        const range = document.createRange();
-        const sel = window.getSelection();
-
-        range.selectNodeContents(input.value);
-        range.collapse(false);
-
-        sel?.removeAllRanges();
-        sel?.addRange(range);
-    })
->>>>>>> a9347fc (vue-version)
 }
 </script>
 
 <template>
-<<<<<<< HEAD
     <div>
         <div class="group">
             <a ref="button"
@@ -73,31 +47,8 @@ function edit() {
             @update="updateButton"
             @close="isPopoverOpen = false"
         />
-=======
-    <div class="group">
-        <button class="button relative px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md"
-                @click="edit()">
-            <edit-button @toggleEditMode="edit()"></edit-button>
-            <div ref="input"
-                 contentEditable="true">{{ buttonText }}</div>
-        </button>
->>>>>>> a9347fc (vue-version)
     </div>
 </template>
 
 <style scoped lang="postcss">
-<<<<<<< HEAD
-
-=======
-input {
-    border: none;
-    margin: 0;
-    padding: 0;
-    background: none;
-    font: inherit;
-    color: inherit;
-    appearance: none;
-    outline: none;
-}
->>>>>>> a9347fc (vue-version)
 </style>
