6fed3a221e1824aafce341ef75b28c8e2c7a9dad
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2023-09-15 19:29:10 +0100

    removed home and profile buttons

--- vue-version/src/components/ImageSearchResults.vue
+++ vue-version/src/components/ImageSearchResults.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
-import type { Basic } from "unsplash-js/dist/methods/photos/types";
-import { computed, ref, watch } from "vue";
-import type { Photos } from "unsplash-js/dist/methods/search/types/response";
-import type { Orientation } from "unsplash-js";
-import { searchPhotos } from "@/services/unsplash";
 
-const { photos: propPhotos, searchQuery: propSearchQuery, orientation: propOrientation } = defineProps<{
-    photos: Photos | undefined;
-    searchQuery: string;
-    orientation?: Orientation;
-}>();
+
+
 const emits = defineEmits(["selectResult", "back"]);
 const searchQuery = ref(propSearchQuery);
 const searchResults = ref(propPhotos?.results ?? []);
@@ -59,68 +51,6 @@ function loadMore() {
 
 <template>
 <div class="flex h-full flex-col space-y-4">
-    <button
-        class="mt-2 w-16 rounded-md bg-gray-300 px-4 py-1 hover:bg-gray-400"
-        @click="emits('back')"
-    >
-        <img
-            src="https://api.iconify.design/mdi:arrow-left-bold.svg?color=%23000000"
-            alt="Back"
-            class="inline-block h-6 w-6"
-        >
-    </button>
-
-    <!-- Search Unsplash -->
-    <div class="mb-4">
-        <label class="mb-1 block font-medium">
-            Search Unsplash:
-            <div class="flex flex-row space-x-4">
-                <input
-                    v-model="searchQuery"
-                    type="text"
-                    class="w-1/2 rounded-md border p-2"
-                    @keydown.enter="search()"
-                >
-                <button
-                    class="rounded-md bg-primary px-4 py-2 text-font hover:opacity-80"
-                    @click="search()"
-                >
-                    Search
-                </button>
-            </div>
-        </label>
-    </div>
-
-    <!-- Display Search Results -->
-    <div
-        v-if="searchResults?.length ?? 0 > 0"
-        class="overflow-y-auto"
-    >
-        <div
-            class="mb-6 mt-4 grid select-none grid-cols-2 space-y-4 text-center"
-        >
-            <div
-                v-for="result in searchResults"
-                :key="result.id"
-                class="mb-2 flex cursor-pointer items-center justify-center overflow-hidden rounded-lg border-4 border-transparent p-1 hover:border-white"
-                @click="selectResult(result)"
-            >
-                <img
-                    :src="result.urls.small"
-                    alt="Unsplash Photo"
-                    class="h-auto max-w-full"
-                >
-            </div>
-        </div>
-        <div class="flex w-full flex-col">
-            <button
-                v-if="currentPage < maxPages"
-                class="button !my-6 mx-auto bg-primary px-10 text-font"
-                @click="loadMore()"
-            >
-                Load More
-            </button>
-        </div>
-    </div>
+    
 </div>
 </template>
