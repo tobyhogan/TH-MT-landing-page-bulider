8dd8981fb4b88d8617fabce3a9179073fc1987f0
Matthias Winkler <github@m-winkler.at>
2023-10-01 20:27:48 +0200

    Rename views to pages

--- vue-version/src/App.vue
+++ vue-version/src/App.vue
@@ -3,14 +3,14 @@ import { ref } from "vue";
 import { RouterView } from "vue-router";
 import { setNewTheme } from "@/services/theme";
 
-document.documentElement.style.setProperty("--color-primary", "#1a6070"); //color that compliments bg e.g. header
+document.documentElement.style.setProperty("--color-primary", "#1a6070"); // color that compliments bg e.g. header
 document.documentElement.style.setProperty("--color-accent", "#6366f1"); // //color for highilghted buttons
 document.documentElement.style.setProperty("--accent-gradient-1", "#111129");
 document.documentElement.style.setProperty("--accent-gradient-2", "#0451b8");
 document.documentElement.style.setProperty("--accent-gradient-3", "#c7ddf2");
 document.documentElement.style.setProperty("--color-background", "#111827"); // majority of the page's color
 document.documentElement.style.setProperty("--color-text", "#dfeaed"); // color for majority of text
-document.documentElement.style.setProperty("--color-title-text", "#1bbf41"); 
+document.documentElement.style.setProperty("--color-title-text", "#1bbf41");
 document.documentElement.style.setProperty("--color-text-accent", "#f3f4f6"); // color for text on highlighted buttons
 document.documentElement.style.setProperty("--dark-mode", "true");
 
--- vue-version/src/main.ts
+++ vue-version/src/main.ts
@@ -4,7 +4,7 @@ import "whatwg-fetch"; // polyfill for fetch, needed for unsplash-js
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/router";
 
 const app = createApp(App);
 
